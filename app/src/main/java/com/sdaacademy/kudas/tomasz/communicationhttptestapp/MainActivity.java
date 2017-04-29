8682febaa3b19e411c137d4914c63402fe4b4ef0
Tomasz Kudas <tomaszkudas@o2.pl>
2017-04-29 10:36:37 +0200

    Prepare MainLayout and single_row layout

--- app/src/main/java/com/sdaacademy/kudas/tomasz/communicationhttptestapp/MainActivity.java
+++ app/src/main/java/com/sdaacademy/kudas/tomasz/communicationhttptestapp/MainActivity.java
@@ -2,12 +2,32 @@ package com.sdaacademy.kudas.tomasz.communicationhttptestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.messageList)
+    ListView messageList;
+
+    @BindView(R.id.messageText)
+    TextView messageText;
+
+    @BindView(R.id.sendMessageButton)
+    Button sendMessageButton;
+
+    @BindView(R.id.getMessageButton)
+    Button getMessageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
